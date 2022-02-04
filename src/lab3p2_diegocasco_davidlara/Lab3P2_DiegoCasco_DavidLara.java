b2fb00197081e169fb18a50d6c300d973ea56d2e
dcasco02 <dcasco@unitec.edu>
2022-02-04 13:51:31 -0600

    Creacion de menu

--- src/lab3p2_diegocasco_davidlara/Lab3P2_DiegoCasco_DavidLara.java
+++ src/lab3p2_diegocasco_davidlara/Lab3P2_DiegoCasco_DavidLara.java
@@ -5,6 +5,8 @@
  */
 package lab3p2_diegocasco_davidlara;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author dcasc
@@ -16,6 +18,27 @@ public class Lab3P2_DiegoCasco_DavidLara {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int opcion=0;
+        while(opcion !=0){
+            opcion = Integer.parseInt(
+            JOptionPane.showInputDialog(
+            "0-Salir\n"
+                    + "1-Crear clase\n"
+                    + "2-Crear ruta\n"
+                    + "3-Crear alumno\n"
+                    + "4-Agregar alumno a clase\n"
+                    + "5-Crear Transportista\n"
+                    + "6-Crear Transporte\n"
+                    + "7- Simulacion\n"
+                    + "8-Listar Clases\n"
+                    + "9-Listar Rutas\n"
+                    + "10-Listar Alumnos\n"
+                    + "11-Listar Transportistas\n"
+                    + "12-Listar Transportes"));
+            if(opcion ==1){
+                
+            }
+        }
     }
     
 }
